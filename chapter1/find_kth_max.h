17d117419b5c2732036b8ca1947b1ab06dc0c25a
buendiya <jingshuaizhen@gmail.com>
2014-01-20 00:44:41 +0800

    Add space after keyword if and for

--- chapter1/find_kth_max.h
+++ chapter1/find_kth_max.h
@@ -5,12 +5,12 @@
 int find_kth_largest_num(int *a, int num, int k)
 {
 	int exchange = 0;
-	for(int i = 0; i<num-1; i++)
+	for (int i = 0; i<num-1; i++)
 	{
 		exchange = 0;
-		for(int j = 0; j<num-1-i; j++)
+		for (int j = 0; j<num-1-i; j++)
 		{
-			if(a[j]>a[j+1])
+			if (a[j]>a[j+1])
 			{
 				int tmp = a[j];
 				a[j]=a[j+1];
@@ -18,7 +18,7 @@ int find_kth_largest_num(int *a, int num, int k)
 				exchange = 1;
 			}
 		}
-		if(0 == exchange)
+		if (0 == exchange)
 		{
 			break;
 		}
@@ -31,12 +31,12 @@ int find_kth_largest_num_2(int *a, int num, int k)
 {
 	//sort the first k elements
 	int exchange = 0;
-	for(int i = 0; i<k-1; i++)
+	for (int i = 0; i<k-1; i++)
 	{
 		exchange = 0;
-		for(int j = 0; j<k-1-i; j++)
+		for (int j = 0; j<k-1-i; j++)
 		{
-			if(a[j]>a[j+1])
+			if (a[j]>a[j+1])
 			{
 				int tmp = a[j];
 				a[j]=a[j+1];
@@ -44,7 +44,7 @@ int find_kth_largest_num_2(int *a, int num, int k)
 				exchange = 1;
 			}
 		}
-		if(0 == exchange)
+		if (0 == exchange)
 		{
 			break;
 		}
@@ -52,13 +52,13 @@ int find_kth_largest_num_2(int *a, int num, int k)
 
 	//As a new element arrives, it is ignored if it is smaller than the kth element in the array.
 	//Otherwise, it is placed in its correct spot in the array, bumping one element out of the array.
-	for(int i = 0; i<num-k; i++)
+	for (int i = 0; i<num-k; i++)
 	{
 		//Get the next (num-k) number in order; Then put the number in the (k+1)th position
 		a[k] = a[k+i];
-		for(int j = k; j>0; j--)  // sort the (k+1) num
+		for (int j = k; j>0; j--)  // sort the (k+1) num
 		{
-			if(a[j]>a[j-1])
+			if (a[j]>a[j-1])
 			{
 				int tmp = a[j];
 				a[j] = a[j-1];
