ee8ccd616f36e2fdece97aba5fe1d8d0336a1fac
buendiya <jingshuaizhen@gmail.com>
2014-02-14 23:07:30 +0800

    Add InsertSort in chapter7/sort.h

--- chapter7/sort.h
+++ chapter7/sort.h
@@ -5,29 +5,36 @@
 
 typedef int ElementType;
 
+void InsertSort(ElementType A[], int N)
+{
+	int i, j;
+	ElementType tmp;
+
+	for (i = 1; i < N; i++)
+	{
+		tmp = A[i];
+		for (j = i; j > 0 && A[j-1] > tmp; j--)
+			A[j] = A[j-1];
+		A[j] = tmp;
+	}
+}
+
 void Shellsort(ElementType A[], int N)
 {
 	int i, j, Increment;
-	ElementType Tmp;
+	ElementType tmp;
 
 	for (Increment = N/2; Increment > 0; Increment /=2)
-		for (i = Increment; i < N; i++)
+	{
+		for (i = Increment; i < N; i += Increment)
 		{
-			Tmp = A[i];
-			for (j = i; j >= Increment; j -= Increment)
-			{
-				printf("\n j = %d, Increment = %d ", j, Increment);
-				if (Tmp < A[j - Increment])
-					A[j] = A[j - Increment];
-				else
-					break;
-			}
-			A[j] = Tmp;
-
-			printf("\nTmp: %d ", Tmp);
-			for (int i = 0; i < N; i++)
-				printf("%d ", A[i]);
+			tmp = A[i];
+			for (j = i; j > 0 && A[j-Increment] > tmp; j -= Increment)
+				A[j] = A[j - Increment];
+			A[j] = tmp;
 		}
+
+	}
 }
 
 #endif
